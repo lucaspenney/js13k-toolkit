732bbe78f171a7a1b5fb56a096d5d7d9b45b6152
Lucas Penney <lucaspenney@gmail.com>
2015-07-28 14:04:40 -0700

    Add file size check as part of compress task, print warning if over 13kb

--- Gruntfile.js
+++ Gruntfile.js
@@ -48,6 +48,15 @@ module.exports = function(grunt) {
 			}
 		},
 		htmlmin: {
+			development: {
+				options: {
+					removeComments: false,
+					collapseWhitespace: false,
+				},
+				files: {
+					'build/index.html': 'src/*.html'
+				}
+			},
 			compressed: {
 				options: {
 					removeComments: true,
@@ -81,9 +90,21 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-htmlmin');
 	grunt.loadNpmTasks('grunt-contrib-compress');
 
+	var fs = require('fs');
+	grunt.registerTask('sizecheck', function() {
+		var done = this.async();
+		fs.stat('build/game.zip', function(err, zip) {
+			if (zip.size > 13312) {
+				//If size in bytes greater than 13kb
+				grunt.log.error("Zipped file greater than 13kb \x07 \n");
+				grunt.log.error("Zip is " + zip.size + " bytes when js13k max is 13,312 bytes");
+			}
+			done();
+		});
+	});
 
 	grunt.registerTask('default', ['watch']);
-	grunt.registerTask('build', ['uglify:development', 'less:development']);
-	grunt.registerTask('build-compress', ['uglify:compressed', 'less:compressed', 'htmlmin:compressed', 'compress:main']);
+	grunt.registerTask('build', ['uglify:development', 'less:development', 'htmlmin:development']);
+	grunt.registerTask('build-compress', ['uglify:compressed', 'less:compressed', 'htmlmin:compressed', 'compress:main', 'sizecheck']);
 
 };
